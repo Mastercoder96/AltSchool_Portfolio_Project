7569c013b4aad014d8dac90bf983859197a8e6b9
Kolawole AKintayo <ykola7@gmail.com>
2024-06-12 20:12:43 +0100

    updating my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import { useState} from 'react';
-import './App.css'
-import {Box, SimpleGrid, Heading, Button, Input } from '@chakra-ui/react'
+import { useState } from "react";
+import "./App.css";
+import {
+  Box,
+  SimpleGrid,
+  Button,
+  Input,
+  Circle,
+  Center,
+  Image,
+  IconButton,
+} from "@chakra-ui/react";
 
-
+import { EmailIcon } from "@chakra-ui/icons";
 
 function App() {
-  
   const [repoData, setRepoData] = useState();
-  
 
-  
-
+  //Get repo data about github user
   async function repoDataURL() {
-    //Get repo data about github user
     await fetch("https://api.github.com/users/Mastercoder96/repos")
       .then((res) => res.json())
       .then(
         (result) => {
           console.log(result);
           const list = result.map((item, i) => (
-            
             <Box key={i}>
               <a target="_blank" href={item.svn_url}>
                 {item.name}
@@ -33,28 +37,54 @@ function App() {
         }
       );
   }
-
-
   return (
-  
-<Box as='body' className='App' display='flex' flexDirection="column" gap= "50px" height= "100%"> 
+    <Box
+      as="body"
+      className="App"
+      display="flex"
+      flexDirection="column"
+      gap="50px"
+      height="100%"
+    >
+      <Center>
+        <Circle size="70px">
+          <Image
+            src="https://avatars.githubusercontent.com/u/69313896?v=4"
+            alt="avatar_url"
+            borderRadius="50%"
+            size="70px"
+          />
+        </Circle>
+        <Box>AKINTAYO KOLAWOLE</Box>
+      </Center>
 
-<Heading as='h2' size='2xl' noOfLines={1}>
-    AKINTAYO'S GITHUB PORTFOLIO
-  </Heading>
-  <Button  onClick={repoDataURL}>Show Repo URL</Button>
+      <Box>
+        Frontend Developer | HTML/CSS | Javascript | Learning React and VueJs
+      </Box>
 
-  <Input variant='filled' placeholder='Search Repository' />
+      <Box>
+      <IconButton
+        colorScheme="teal"
+        aria-label="Send email"
+        p= "15px"
+        w= "10px"
+        icon={<EmailIcon />}
+      />
 
- 
-  <SimpleGrid spacing={200} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-  
-  
-</SimpleGrid>
- {repoData}
-</Box>  
+      </Box>
 
-   
+
+
+      <Input variant="filled" placeholder="Search Repository" />
+
+      <Button onClick={repoDataURL}>Show Repo URL</Button>
+
+      <SimpleGrid
+        spacing={200}
+        templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+      ></SimpleGrid>
+      {repoData}
+    </Box>
   );
 }
 
